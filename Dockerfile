929702cbd528f39fe9a579948b8f3ff5e6f23432
DenDen047 <sh.mn.nat@gmail.com>
2021-09-16 22:42:51 +0900

    prepared the env for gpu

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,12 @@
-FROM julia:1.6.2
+FROM nvidia/cuda:11.4.2-cudnn8-runtime-ubuntu20.04
 
-# Pluto
+RUN apt-get update && apt-get upgrade -y
+
+# julia
+RUN apt-get install -y python3-pip
+RUN pip3 install jill -U
+RUN echo "Y" | jill install 1.6.2
+RUN apt-get install -y curl wget
+
 RUN julia -e 'using Pkg; Pkg.add(["Pluto"])'
+RUN julia -e 'using Pkg; Pkg.add(["Flux", "CUDA"])'
--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 CURRENT_PATH=$(pwd)
-IMAGE_NAME="denden047/julia"
+IMAGE_NAME="denden047/julia_gpu"
 
 docker build -t ${IMAGE_NAME} . && \
 docker run -it --rm \
+    --gpus 1 \
     -p 1234:1234 \
     -v $(pwd):/workdir \
     -w /workdir \
     ${IMAGE_NAME} \
-    /usr/local/julia/bin/julia -e 'using Pluto; Pluto.run(host="0.0.0.0")'
+    /usr/local/bin/julia -e 'using Pluto; Pluto.run(host="0.0.0.0")'
